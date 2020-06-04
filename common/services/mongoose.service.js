2bae05e015b7626c0e0e1900e22214d05183441a
Obiorah Cyril <obiorah@softcom.ng>
2020-06-04 13:24:20 +0100

    remove redundant lines

--- common/services/mongoose.service.js
+++ common/services/mongoose.service.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 let count = 0;
 
-// const uri = 'mongodb://heroku_fb6s18j2:password4password@ds033106.mlab.com:33106/heroku_fb6s18j2';
-// const uri = 'mongodb+srv://cyobiorah:passwordispassword@market-boy-98vqo.mongodb.net/market-boy?retryWrites=true&w=majority';
 const uri = 'mongodb://127.0.0.1/market-boy';
 
 const options = {
@@ -16,7 +14,6 @@ const options = {
 
 const connectWithRetry = () => {
     console.log('MongoDB connection with retry')
-    // mongoose.connect(process.env.MONGODB_URI, options).then(() => {
     mongoose.connect(uri, options).then(() => {
         console.log('MongoDB is connected')
     }).catch(err => {
